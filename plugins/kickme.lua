4478f09349a42b9481301a28e219ec54950c738f
MaSkAoS <atilita@gmail.com>
2016-02-06 22:17:05 +0100

    Update kickme.lua

--- plugins/kickme.lua
+++ plugins/kickme.lua
@@ -2,7 +2,7 @@ local function kick_user(msg)
     local chat = 'chat#id'..msg.to.id
     local channel = 'channel#id'..msg.to.id
     local user = msg.from.id
-    send_msg('channel#id'..msg.to.id, ' @'..msg.from.username..'  ( Id:  '..msg.from.id..' ) Vuelve pronto .', ok_cb, true)
+    send_msg('channel#id'..msg.to.id, ' @'..msg.from.username..'  ( Id:  '..msg.from.id..' ) Bye .', ok_cb, true)
     if msg.to.type == 'chat' then
         chat_del_user(chat, 'user#id'..user, ok_cb, true)
     elseif msg.to.type == 'channel' then
@@ -12,11 +12,8 @@ end
 
 local function run(msg)
     kick_user(msg)
-    delete_msg(msg.id, ok_cb, false)
-    --send_report(msg)
 end
 
 return {patterns = {
-    -- You can add much as patterns you want to stop all spam traffic
     '!kickme'
 }, run = run}
