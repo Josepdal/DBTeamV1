db1932671a9d96401efcdfacc2d6bd47dc96306b
Josep Dalmau <josepdal97@gmail.com>
2016-02-08 21:19:32 +0100

    "First Commit"

--- bot/bot.lua
+++ bot/bot.lua
@@ -207,29 +207,9 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
-      "9gag",
-      "eur",
-      "echo",
-      "btc",
-      "get",
-      "giphy",
-      "google",
-      "gps",
-      "help",
-      "id",
-      "images",
-      "img_google",
-      "location",
-      "media",
       "plugins",
-      "channels",
-      "set",
-      "stats",
-      "time",
-      "version",
-      "weather",
-      "xkcd",
-      "youtube" },
+      "settings",
+      "id" },
     sudo_users = {our_id},
     disabled_channels = {}
   }
