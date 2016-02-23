265df311f595ad2c46337f2d550d22b994f418ee
Josep Dalmau <josepdal97@gmail.com>
2016-02-24 00:23:00 +0100

    Multi-language and permissions support

--- plugins/commands.lua
+++ plugins/commands.lua
@@ -12,62 +12,61 @@
 --------------------------------------------------
 
 do
-
--- Returns true if is not empty
-local function has_usage_data(dict)
-  if (dict.usage == nil or dict.usage == '') then
-    return false
-  end
-  return true
-end
-
--- Get commands for that plugin
-local function plugin_help(name)
-  local plugin = plugins[name]
-  if not plugin then return nil end
-
-  local text = ""
-  if (type(plugin.usage) == "table") then
-    for ku,usage in pairs(plugin.usage) do
-      text = text..usage..'\n'
+    local function run(msg, matches)
+        text = '#⃣ '..lang_text(msg.to.id, 'commandsT')..':\n'
+        local space = '\n'
+        if matches[1] == 'commands' and not matches[2] then
+            if permissions(msg.from.id, msg.to.id, "mod_commands") then
+                local langHash = 'langset:'..msg.to.id
+                local lang = redis:get(langHash)
+                for v,plugin in pairs(_config.enabled_plugins) do
+                    local textHash = 'lang:'..lang..':'..plugin..':0'
+                    if redis:get(textHash) then
+                        for i=1, tonumber(lang_text(msg.to.id, plugin..':0')), 1 do
+                            text = text..lang_text(msg.to.id, plugin..':'..i)..'\n'
+                        end
+                        text = text..space
+                    end
+                end
+            else
+                text = text..lang_text(msg.to.id, 'moderation:5')..'\n'
+                text = text..lang_text(msg.to.id, 'version:1')..'\n'
+                text = text..lang_text(msg.to.id, 'rules:1')..'\n'
+            end
+        elseif matches[1] == 'commands' and matches[2] then
+            if permissions(msg.from.id, msg.to.id, "mod_commands") then
+                local langHash = 'langset:'..msg.to.id
+                local lang = redis:get(langHash)
+                for v,plugin in pairs(_config.enabled_plugins) do
+                    if plugin == matches[2] then
+                        local textHash = 'lang:'..lang..':'..plugin..':0'
+                        if redis:get(textHash) then
+                            for i=1, tonumber(lang_text(msg.to.id, plugin..':0')), 1 do
+                                text = text..lang_text(msg.to.id, plugin..':'..i)..'\n'
+                            end
+                        end
+                        return text
+                    end
+                end
+                return 'ℹ️ '..lang_text(msg.to.id, 'errorNoPlug')
+            else
+                return '🚫 '..lang_text(msg.to.id, 'require_mod')
+            end
+        end
+        return text
     end
-    text = text..'\n'
-  elseif has_usage_data(plugin) then -- Is not empty
-    text = text..plugin.usage..'\n\n'
-  end
-  return text
+
+    return {
+        patterns = {
+            "^#(commands)$",
+            "^#(commands) (.+)"
+        }, 
+        run = run 
+    }
 end
 
--- !help all command
-local function help_all()
-  local ret = ""
-  for name in pairs(plugins) do
-    ret = ret .. plugin_help(name) .. '======================\n'
-  end
-  return ret
-end
-
-local function run(msg, matches)
-  if permissions(msg.from.id, msg.to.id, "commands") then
-    if matches[1] == "#commands" then
-      return help_all()
-    else 
-      local text = '© Commands for '..matches[1]..':\n'
-      local text = text..plugin_help(matches[1])
-      if not text then
-        text = telegram_help()
-      end
-      return text
+for v,user in pairs(_gbans.gbans_users) do
+    if tonumber(user) == tonumber(user_id) then
+        return true
     end
   end
-end
-
-return {
-  patterns = {
-    "^#commands$",
-    "^#commands (.+)"
-  }, 
-  run = run 
-}
-
-end
