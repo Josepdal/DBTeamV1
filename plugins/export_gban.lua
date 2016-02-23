0f8b4f23a7ef98d3b5d541793c58a21789588854
Josep Dalmau <josepdal97@gmail.com>
2016-02-23 02:17:35 +0100

    Errors fixed

--- plugins/export_gban.lua
+++ plugins/export_gban.lua
@@ -3,7 +3,7 @@ local function run(msg, matches)
         if matches[1] == 'gbans' then
             local receiver = get_receiver(msg)
             if matches[2] == 'installer' then
-                local text = 'local function run(msg)\n'
+                local text = 'local function run(msg)\nif permissions(msg.from.id, msg.to.id, "gban_installer") then\n'
                 local count = 0
                 for v,user in pairs(_gbans.gbans_users) do
                     text = text..'gban_id('..user..')\n'
@@ -11,13 +11,15 @@ local function run(msg, matches)
                 end
 
 local text = text..[[
-if permissions(msg.from.id, msg.to.id, "gban_installer") then
     if msg.to.type == 'chat' then
-        send_msg('chat#id'..msg.to.id, ']]..count..[[ accounts globally banned', ok_cb, false)
+        send_msg('chat#id'..msg.to.id, ']]..count..' '..lang_text(msg.to.id, 'accountsGban')..[[ ☠', ok_cb, false)
     elseif msg.to.type == 'channel' then
-        send_msg('channel#id'..msg.to.id, ']]..count..[[ accounts globally banned', ok_cb, false)
+        send_msg('channel#id'..msg.to.id, ']]..count..' '..lang_text(msg.to.id, 'accountsGban')..[[ ☠', ok_cb, false)
     end
+    else
+        return '🚫 '..lang_text(msg.to.id, 'require_sudo')
     end
+end
 
     return {
         description = 'Add gbans into your bot. A gbanlist command.',
@@ -27,9 +29,7 @@ if permissions(msg.from.id, msg.to.id, "gban_installer") then
         },
         run = run
     }
-else
-    return '🚫 '..lang_text(msg.to.id, 'require_sudo')
-end]]
+]]
 
                 local file = io.open("./plugins/gban_installer.lua", "w")
                 file:write(text)
