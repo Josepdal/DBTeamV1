9510e354f63a190abf2b3a692b54d1817138b22c
MaSkAoS <atilita@gmail.com>
2016-02-26 18:09:09 +0100

    Update english_lang.lua

--- plugins/english_lang.lua
+++ plugins/english_lang.lua
@@ -40,7 +40,7 @@ local function run(msg, matches)
 		set_text(LANG, 'noStickersT', 'Stickers are not allowed in this chat.')
 		set_text(LANG, 'noStickersL', 'Stickers are not allowed in this supergroup.')
 		set_text(LANG, 'stickersT', 'Stickers are now allowed in this chat.')
-		set_text(LANG, 'stickersL', 'Stickers are not allowed in this supergroup.')
+		set_text(LANG, 'stickersL', 'Stickers are now allowed in this supergroup.')
 
 		set_text(LANG, 'gifsT', 'Gifs are now allowed in this chat.')
 		set_text(LANG, 'gifsL', 'Gifs are now allowed in this supergroup.')
