7b01e46b2082330ee7ad19ba1ccd34f0510465f0
Josep Dalmau <josepdal97@gmail.com>
2016-02-26 18:33:57 +0100

    fix #id command

--- plugins/id.lua
+++ plugins/id.lua
@@ -91,13 +91,14 @@ end
 
 local function run(msg, matches)
     local receiver = get_receiver(msg)
+    local chat = msg.to.id
     -- Id of the user and info about group / channel
     if matches[1] == "#id" then
         if permissions(msg.from.id, msg.to.id, "id") then
             if msg.to.type == 'channel' then
-                send_msg(msg.to.peer_id, '🔠 '..lang_text(chat, 'supergroupName')..' :'..msg.to.print_name:gsub("_", " ")..'\n👥 '..lang_text(chat, 'supergroup')..' ID: '..msg.to.id..'\n🆔 '..lang_text(chat, 'user')..' ID: '..msg.from.id, ok_cb, false)
+                send_msg(msg.to.peer_id, '🔠 '..lang_text(chat, 'supergroupName')..': '..msg.to.print_name:gsub("_", " ")..'\n👥 '..lang_text(chat, 'supergroup')..' ID: '..msg.to.id..'\n🆔 '..lang_text(chat, 'user')..' ID: '..msg.from.id, ok_cb, false)
             elseif msg.to.type == 'chat' then
-                send_msg(msg.to.peer_id, '🔠 '..lang_text(chat, 'chatName')..' :'..msg.to.print_name:gsub("_", " ")..'👥 '..lang_text(chat, 'chat')..' ID: '..msg.to.id..'\n'..'\n🆔 '..lang_text(chat, 'user')..' ID: '..msg.from.id, ok_cb, false)
+                send_msg(msg.to.peer_id, '🔠 '..lang_text(chat, 'chatName')..': '..msg.to.print_name:gsub("_", " ")..'\n👥 '..lang_text(chat, 'chat')..' ID: '..msg.to.id..'\n🆔 '..lang_text(chat, 'user')..' ID: '..msg.from.id, ok_cb, false)
             end
         end
     elseif matches[1] == 'whois' then
