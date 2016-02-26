e3d4c1760e89ff069eb9c553441ab0372b96bd08
Josep Dalmau <josepdal97@gmail.com>
2016-02-27 00:58:53 +0100

    Modify version

--- plugins/version.lua
+++ plugins/version.lua
@@ -14,7 +14,7 @@
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/DBTeam-ES/DBTeam-Bot\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@skneos\n@Thef7HD' 
+  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/Josepdal/DBTeam\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@skneos\n@iicc1\n@Serx666' 
   --[[ Checkout http://github.com/Desarrollo-Bots-Telegram-ES/DBTeam-Bot
   --GNU GPL v2 license.]]
 end
