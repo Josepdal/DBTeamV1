2ec544fad138d48d7c4582b9d70c4b4e1d911028
MaSkAoS <atilita@gmail.com>
2016-02-27 15:09:22 +0100

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -7,7 +7,7 @@
 --------------------------------------------------
 --                                              --
 --       Developers: @Josepdal & @MaSkAoS       --
---     Support: @Skneos , @iicc1 & @serx666     --
+--     Support: @Skneos,  @iicc1 & @serx666     --
 --                                              --
 --------------------------------------------------
 
@@ -15,7 +15,7 @@ do
 
 function run(msg, matches)
   return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/Josepdal/DBTeam\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@skneos\n@iicc1\n@Serx666' 
-  --[[ Checkout http://github.com/Desarrollo-Bots-Telegram-ES/DBTeam-Bot
+  --[[ Checkout https://github.com/Josepdal/DBTeam
   --GNU GPL v2 license.]]
 end
 
