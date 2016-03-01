fd2821574bc37a41eebac542a775e9428486cf4d
Josep Dalmau <josepdal97@gmail.com>
2016-03-01 02:26:21 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit de355e7355e3aab6364851ce1fbc8621c40ea8a9, reversing
    changes made to e54afa3f01980fc03e9f483ed5f1148aa805a65b.

--- plugins/rules.lua
+++ plugins/rules.lua
@@ -12,9 +12,7 @@
 --			Created by @Josepdal & @A7F			--
 --												--
 --------------------------------------------------
-first_mod=true
 
-<<<<<<< HEAD
 local function set_rules_channel(msg, text)
   	local rules = text
   	local hash = 'channel:id:'..msg.to.id..':rules'
@@ -47,50 +45,11 @@ local function ret_rules_channel(msg)
   		init_def_rules(chat_id)
   		return redis:get(hash)
   	end
-=======
-local function set_rules_channel(msg,text)
-  local rules = text
-  local hash="channel:id"..msg.to.id.."rules"
-  redis:set(hash,rules)
-  return "ok! Rules are ready"
-end
-
-local function del_rules_channel(msg)
-  local hash="channel:id"..msg.to.id.."rules"
-  redis:del(hash)
-  return "ok! Rules are gone"
-end
-
-local function init_def_rules(id)
-  local rules=" ℹ️ Rules:\n"
-              .." 1⃣ No Flood.\n"
-              .." 2⃣ No Spam.\n"
-              .." 3⃣ Try to stay on topic.\n"
-              .." 4⃣ Forbidden any racist, sexual, homophobic or gore content.\n"
-              .." ➡️ Repeated failure to comply with these rules will cause ban."
-              
-  local hash="channel:id"..id.."rules"
-  redis:set(hash,rules)
-  first_mod=false
-end
-
-local function ret_rules_channel(msg)
-  local id=msg.to.id
-  local hash="channel:id"..msg.to.id.."rules"
-  local rules=redis:get(hash)
-  if (not rules) and first_mod then
-    init_def_rules(id)
-  elseif not rules then
-    return "there are no rules here. Anarchy in the USA!"
-  end
-  return rules
->>>>>>> origin/master
 end
 
 
 local function run(msg, matches)
   
-<<<<<<< HEAD
     if matches[1] == 'rules' then
       	return ret_rules_channel(msg)
     elseif matches[1] == 'setrules' then
@@ -105,38 +64,13 @@ local function run(msg, matches)
     	end
     end
     
-=======
-    if matches[1]=="rules" then
-      return ret_rules_channel(msg)
-    end
-    
-    if permissions(msg.from.id, msg.to.id, "rules") then
-      if matches[1]=="setrules" then
-        return set_rules_channel(msg,matches[2])
-      elseif matches[1]=="remrules" then
-        return del_rules_channel(msg)
-      else return "nope"
-      end
-    end
-    
-    return "sry, you failed lmao"
->>>>>>> origin/master
 end
 
 return {
   patterns = {
-<<<<<<< HEAD
     '^#(rules)$',
     '^#(setrules) (.+)$',
     '^#(remrules)$'
   }, 
   run = run 
 }
-=======
-    "^#(rules)$",
-    "^#(setrules) (.+)$",
-    "^#(remrules)$"
-  }, 
-  run = run 
-}
->>>>>>> origin/master
