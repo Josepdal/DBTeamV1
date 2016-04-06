c036588e659a7fed20b5589615940c1530b0f088
jarriztg <rickygamberroz@hotmail.com>
2016-04-06 06:11:54 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,7 +17,8 @@ echo "   1) Iniciar DBTeam (tmux)."
 echo "   2) Abrir sesion anterior (tmux)."
 echo "   3) Cerrar todas las sesiones."
 echo "   4) Reiniciar DBTeam (tmux)."
-echo "   5) Salir."
+echo "   5) Actualizar DBTeam."
+echo "   6) Salir."
 echo -e "\e[30m"
 read VAR
 if [ "$VAR" = 1 ]; then
@@ -34,8 +35,12 @@ echo Sesiones cerradas.
 echo -e "\e[30m"
 elif [ "$VAR" = 4 ]; then
 killall tmux
+read -t5 -n1 -r -p 'Presiona cualquier tecla para continuar...' 
 tmux new-session -s script "bash steady.sh -t" 
 elif [ "$VAR" = 5 ]; then
+clear
+git pull
+elif [ "$VAR" = 6 ]; then
 clear
 exit
 elif [ "$VAR" = "" ]; then
@@ -54,13 +59,14 @@ elif [ "$VAR" = 1 ]; then
 
 clear
 echo -e "\e[34m"
-echo "Please, select your option."
+echo "Por favor, selecciona tu opcion."
 echo -e "\033[38;5;208m"
 echo "   1) Run DBTeam (tmux)."
 echo "   2) Open old session (tmux)."
 echo "   3) Close all sessions."
 echo "   4) Restart DBTeam (tmux)."
-echo "   5) Exit."
+echo "   5) Update DBTeam."
+echo "   6) Exit."
 echo -e "\e[30m"
 read VAR
 if [ "$VAR" = 1 ]; then
@@ -78,8 +84,12 @@ echo -e "\e[30m"
 echo
 elif [ "$VAR" = 4 ]; then
 killall tmux
+read -t5 -n1 -r -p 'Press any key to continue...' 
 tmux new-session -s script "bash steady.sh -t" 
 elif [ "$VAR" = 5 ]; then
+clear
+git pull
+elif [ "$VAR" = 6 ]; then
 clear
 exit
 elif [ "$VAR" = "" ]; then
