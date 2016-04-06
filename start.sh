a736553805227aba11a90855e9aa4cb02d258e62
jarriztg <rickygamberroz@hotmail.com>
2016-04-06 07:39:41 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,13 +7,13 @@ echo -e "     | |_  )  _ \ | |/ .__|  _ \_|  \/  |     "
 echo -e "     |____/|____/ |_|\____/\_____|_/\/\_|     "
 echo -e "                                              \033[0;00m"
 echo -e "          Developers @Josepdal @MaSkAoS       "
-echo -e "   steady.sh by @iicc1    start.sh by @Jarriz  "
+echo -e "      steady @iicc1         start by @Jarriz  "
 echo -e "\e[34m"
 echo "Please, select your language."
 echo -e "\033[38;5;208m"
 echo "   1) English."
 echo "   2) Spanish."
-echo -e "\e[30m"
+echo -e "\e[32m"
 read VAR
 if [ "$VAR" = 2 ]; then
 
@@ -28,7 +28,7 @@ echo "   4) Cerrar todas las sesiones."
 echo "   5) Reiniciar DBTeam (tmux)."
 echo "   6) Actualizar DBTeam."
 echo "   7) Salir."
-echo -e "\e[30m"
+echo -e "\e[32m"
 read VAR
 if [ "$VAR" = 1 ]; then
 tmux new-session -s script "bash steady.sh -t" 
@@ -43,7 +43,7 @@ killall tmux
 clear
 echo -e "\e[34m"
 echo Sesiones cerradas.
-echo -e "\e[30m"
+echo -e "\e[32m"
 elif [ "$VAR" = 5 ]; then
 killall tmux
 read -t5 -n1 -r -p 'Presiona cualquier tecla para continuar...' 
@@ -58,28 +58,28 @@ elif [ "$VAR" = "" ]; then
 clear
 echo -e "\e[31m"
 echo "Opcion invalida"
-echo -e "\e[30m"
+echo -e "\e[32m"
 else
 clear
 echo -e "\e[31m"
 echo "Opcion invalida"
-echo -e "\e[30m"
+echo -e "\e[32m"
 fi
 
 elif [ "$VAR" = 1 ]; then
 
 clear
 echo -e "\e[34m"
-echo "Por favor, selecciona tu opcion."
+echo "Please, select your option."
 echo -e "\033[38;5;208m"
 echo "   1) Run DBTeam (tmux)."
-echo "   2) Open old session (register)."
-echo "   3) Open old session (tmux)."
+echo "   2) Last session (register)."
+echo "   3) Last session (tmux)."
 echo "   4) Close all sessions."
 echo "   5) Restart DBTeam (tmux)."
 echo "   6) Update DBTeam."
 echo "   7) Exit."
-echo -e "\e[30m"
+echo -e "\e[32m"
 read VAR
 if [ "$VAR" = 1 ]; then
 tmux new-session -s script "bash steady.sh -t" 
@@ -94,7 +94,7 @@ killall tmux
 clear
 echo -e "\e[34m"
 echo Sessions closed.
-echo -e "\e[30m"
+echo -e "\e[32m"
 echo
 elif [ "$VAR" = 5 ]; then
 killall tmux
@@ -110,22 +110,22 @@ elif [ "$VAR" = "" ]; then
 clear
 echo -e "\e[31m"
 echo "Option invalid"
-echo -e "\e[30m"
+echo -e "\e[32m"
 else
 clear
 echo -e "\e[31m"
 echo "Option invalid"
-echo -e "\e[30m"
+echo -e "\e[32m"
 fi
 
 elif [ "$VAR" = "" ]; then
 clear
 echo -e "\e[31m"
 echo "Option invalid"
-echo -e "\e[30m"
+echo -e "\e[32m"
 else
 clear
 echo -e "\e[31m"
 echo "Option invalid"
-echo -e "\e[30m"
+echo -e "\e[32m"
 fi
