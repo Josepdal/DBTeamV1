416622527d1d488a734a774020d136a2546e7ce1
jarriztg <rickygamberroz@hotmail.com>
2016-04-06 06:40:05 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -22,18 +22,21 @@ echo -e "\e[34m"
 echo "Por favor, selecciona tu opcion."
 echo -e "\033[38;5;208m"
 echo "   1) Iniciar DBTeam (tmux)."
-echo "   2) Abrir sesion anterior (tmux)."
-echo "   3) Cerrar todas las sesiones."
-echo "   4) Reiniciar DBTeam (tmux)."
-echo "   5) Actualizar DBTeam."
-echo "   6) Salir."
+echo "   2) Abrir sesion anterior (registros)"
+echo "   3) Abrir sesion anterior (tmux)."
+echo "   4) Cerrar todas las sesiones."
+echo "   5) Reiniciar DBTeam (tmux)."
+echo "   6) Actualizar DBTeam."
+echo "   7) Salir."
 echo -e "\e[30m"
 read VAR
 if [ "$VAR" = 1 ]; then
 tmux new-session -s script "bash steady.sh -t" 
 elif [ "$VAR" = 2 ]; then
-tmux attach-session -t script
+tmux attach-session -t DBTeam
 elif [ "$VAR" = 3 ]; then
+tmux attach-session -t script
+elif [ "$VAR" = 4 ]; then
 killall screen
 killall telegram-cli
 killall tmux
@@ -41,14 +44,14 @@ clear
 echo -e "\e[34m"
 echo Sesiones cerradas.
 echo -e "\e[30m"
-elif [ "$VAR" = 4 ]; then
+elif [ "$VAR" = 5 ]; then
 killall tmux
 read -t5 -n1 -r -p 'Presiona cualquier tecla para continuar...' 
 tmux new-session -s script "bash steady.sh -t" 
-elif [ "$VAR" = 5 ]; then
+elif [ "$VAR" = 6 ]; then
 clear
 git pull
-elif [ "$VAR" = 6 ]; then
+elif [ "$VAR" = 7 ]; then
 clear
 exit
 elif [ "$VAR" = "" ]; then
@@ -70,18 +73,21 @@ echo -e "\e[34m"
 echo "Por favor, selecciona tu opcion."
 echo -e "\033[38;5;208m"
 echo "   1) Run DBTeam (tmux)."
-echo "   2) Open old session (tmux)."
-echo "   3) Close all sessions."
-echo "   4) Restart DBTeam (tmux)."
-echo "   5) Update DBTeam."
-echo "   6) Exit."
+echo "   2) Open old session (register)."
+echo "   3) Open old session (tmux)."
+echo "   4) Close all sessions."
+echo "   5) Restart DBTeam (tmux)."
+echo "   6) Update DBTeam."
+echo "   7) Exit."
 echo -e "\e[30m"
 read VAR
 if [ "$VAR" = 1 ]; then
 tmux new-session -s script "bash steady.sh -t" 
 elif [ "$VAR" = 2 ]; then
-tmux attach-session -t script
+tmux attach-session -t DBTeam
 elif [ "$VAR" = 3 ]; then
+tmux attach-session -t script
+elif [ "$VAR" = 4 ]; then
 killall screen
 killall telegram-cli
 killall tmux
@@ -90,14 +96,14 @@ echo -e "\e[34m"
 echo Sessions closed.
 echo -e "\e[30m"
 echo
-elif [ "$VAR" = 4 ]; then
+elif [ "$VAR" = 5 ]; then
 killall tmux
 read -t5 -n1 -r -p 'Press any key to continue...' 
 tmux new-session -s script "bash steady.sh -t" 
-elif [ "$VAR" = 5 ]; then
+elif [ "$VAR" = 6 ]; then
 clear
 git pull
-elif [ "$VAR" = 6 ]; then
+elif [ "$VAR" = 7 ]; then
 clear
 exit
 elif [ "$VAR" = "" ]; then
