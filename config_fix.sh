d5712c7e9a9bba7f7586289c11dc2858ee6fa6d6
Ignacio Iglesias <ignacio.iglesias@hotmail.es>
2016-04-10 01:50:34 +0200

    fix

--- config_fix.sh
+++ config_fix.sh
@@ -5,12 +5,6 @@
 CONF=data/config.lua
 CONF2=data/config2.lua
 
-if [ -f "$CONF" ]; then
-   echo "Config.lua found"
-else
-   exit 0
-fi
-
 cat $CONF | grep "enabled_lang" > /dev/null
 if [ $? == 0 ]; then
 	echo -e "\e[33mPatching config.lua not needed.\e[36m"
@@ -19,26 +13,21 @@ else
 	
 	echo -e "\e[33mPatching config.lua languages...\e[39m"
 	sleep 1.5
+	
+	#config.lua backup
+	cp $CONF ${CONF}.backup
+	
 	grep -v "_lang" $CONF > $CONF2
 
-	awk '/sudo_users/{print "  enabled_lang = {"}1' $CONF > $CONF2
-	mv $CONF2 $CONF
+	awk '/sudo_users/{print "  enabled_lang = {"}1' $CONF2 > $CONF
 	sed 's/.*enabled_lang.*/&\n    "arabic_lang",/' $CONF > $CONF2
-	mv $CONF2 $CONF
-	sed 's/.*arabic_lang.*/&\n    "catalan_lang",/' $CONF > $CONF2
-	mv $CONF2 $CONF
+	sed 's/.*arabic_lang.*/&\n    "catalan_lang",/' $CONF2 > $CONF
 	sed 's/.*catalan_lang.*/&\n    "english_lang",/' $CONF > $CONF2
-	mv $CONF2 $CONF
-	sed 's/.*english_lang.*/&\n    "galician_lang",/' $CONF > $CONF2
-	mv $CONF2 $CONF
+	sed 's/.*english_lang.*/&\n    "galician_lang",/' $CONF2 > $CONF
 	sed 's/.*galician_lang.*/&\n    "italian_lang",/' $CONF > $CONF2
-	mv $CONF2 $CONF
-	sed 's/.*italian_lang.*/&\n    "persian_lang",/' $CONF > $CONF2
-	mv $CONF2 $CONF
+	sed 's/.*italian_lang.*/&\n    "persian_lang",/' $CONF2 > $CONF
 	sed 's/.*persian_lang.*/&\n    "portuguese_lang",/' $CONF > $CONF2
-	mv $CONF2 $CONF
-	sed 's/.*portuguese_lang.*/&\n    "spanish_lang"/' $CONF > $CONF2
-	mv $CONF2 $CONF
+	sed 's/.*portuguese_lang.*/&\n    "spanish_lang"/' $CONF2 > $CONF
 	sed 's/.*spanish_lang.*/&\n  },/' $CONF > $CONF2
 	mv $CONF2 $CONF
 	echo -e "\e[96mDone!\e[36m"
