327af18527de047ef1f76cbb21e37fc4d56bf453
jarriztg <rickygamberroz@hotmail.com>
2016-04-18 05:47:45 -0500

    Update links.lua

--- plugins/links.lua
+++ plugins/links.lua
@@ -1,11 +1,11 @@
-local function remove_message(msg)
+local function kick_user(msg)
     local chat = 'chat#id'..msg.to.id
     local channel = 'channel#id'..msg.to.id
     local user = msg.from.id
     if msg.to.type == 'chat' then
         chat_del_user(chat, 'user#id'..user, ok_cb, true)
     elseif msg.to.type == 'channel' then
-        channel_remove_message(channel, 'user#id'..user, ok_cb, true)
+        channel_kick_user(channel, 'user#id'..user, ok_cb, true)
     end
 end
 
