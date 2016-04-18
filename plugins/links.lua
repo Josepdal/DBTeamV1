3d1ef516c4b1ea2788c7c16c275d41ed81daae0a
jarriztg <rickygamberroz@hotmail.com>
2016-04-18 04:55:23 -0500

    Update links.lua
    esto es a tu criterio :D cierra los que menos consideres innecesarios

--- plugins/links.lua
+++ plugins/links.lua
@@ -1,11 +1,11 @@
-local function kick_user(msg)
+local function remove_message(msg)
     local chat = 'chat#id'..msg.to.id
     local channel = 'channel#id'..msg.to.id
     local user = msg.from.id
     if msg.to.type == 'chat' then
         chat_del_user(chat, 'user#id'..user, ok_cb, true)
     elseif msg.to.type == 'channel' then
-        channel_kick_user(channel, 'user#id'..user, ok_cb, true)
+        channel_remove_message(channel, 'user#id'..user, ok_cb, true)
     end
 end
 
@@ -13,7 +13,6 @@ local function run(msg, matches)
     if not permissions(msg.from.id, msg.to.id, "settings") then
         local hash = 'links:'..msg.to.id
         if redis:get(hash) then
-            kick_user(msg)
             delete_msg(msg.id, ok_cb, false)
         end
     end
