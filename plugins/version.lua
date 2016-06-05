49e41d93595eb11d6dfe19157f2e6b1dd445256a
sheckesper <ibrahimlovers74@gmail.com>
2016-06-05 19:31:25 +0300

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,7 +1,7 @@
 do
 
 function user99(msg, matches)
-  return "Bbabeleon bot 1.0 V  🇮🇶\n#English 🔌🌝\n\n#Developer 🕵\n@lock_at_me_now 🔧😷\n\nMy Telegram Channel 📡\nhttps://telegram.me/joinchat/Ctyr5z9lsYfvKpfoQgssww"
+  return "Bbabeleon bot 1.0 V  🇮🇶\n#English 🔌🌝\n\n#Developer 🕵\n@lock_at_me_now 🔧😷\n\nMy Telegram Channel 📡\nhttps://telegram.me/babeleon_b0t"
 end
 
 return {
